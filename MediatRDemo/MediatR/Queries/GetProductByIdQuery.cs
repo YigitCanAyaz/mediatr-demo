f48015f9ff8f25e25574f0b5345d1c0091294491
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2023-01-12 11:44:33 +0300

    Add project files.

---
+++ MediatRDemo/MediatR/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace MediatRDemo.MediatR.Queries
+{
+    public class GetProductByIdQuery : IRequest<GetProductViewModel>
+    {
+        public Guid Id { get; set; } 
+    }
+}
